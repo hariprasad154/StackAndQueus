afc19b1cb9899f49ccadc5b6951801a57ea00cd4
Hari Prasad G <hariprasas154@gmail.com>
2023-02-01 23:37:53 +0530

    added

--- src/Com/Queue/Queue.java
+++ src/Com/Queue/Queue.java
@@ -1,5 +1,30 @@
 package Com.Queue;
 
-public class Queue {
+
+
+public class Queue<T> {
+	MyLinkedList<T> linkedList;
+
+	public Queue() {
+		linkedList = new MyLinkedList<>();
+	}
+	public void enqueue(T data) {
+		linkedList.Append(data);
+	}
+	public  void Dequeue() {
+		
+		MyNode<T> temp = linkedList.head;
+		while(temp.next !=linkedList.tail) {
+			temp=temp.next;
+		}
+		temp.next=null;
+		temp =linkedList.tail;
+		
+	}
+	
+	public void display() {
+		linkedList.display();
+	}
+
 
 }
